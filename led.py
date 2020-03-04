5c941b3fd6313f3b3cb64a3ae0ac1a113dc0002d
mirek <zdenek.devaty@gmail.com>
2020-03-04 14:36:20 +0100

    Add led.py v2

---
+++ led.py
@@ -0,0 +1,14 @@
+#Importing GPIO and sleep libraries
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+while(True):
+    GPIO.output(7, GPIO.HIGH)
+    sleep(1)
+    GPIO.output(7, GPIO.LOW)
+    sleep(1)
+
+GPIO.cleanup()
